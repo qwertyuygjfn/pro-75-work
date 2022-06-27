dcdd1aeaad021e1f1bd070504e81a81bef86decd
qwertyuygjfn <102384707+qwertyuygjfn@users.noreply.github.com>
2022-06-27 12:40:17 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAsdYcyG9kJIFM48raKiCpfzVfgFdlcIio",
+  authDomain: "pro74and75.firebaseapp.com",
+  databaseURL: "https://pro74and75-default-rtdb.firebaseio.com",
+  projectId: "pro74and75",
+  storageBucket: "pro74and75.appspot.com",
+  messagingSenderId: "132775329858",
+  appId: "1:132775329858:web:92dd2f3d6ecee9237caddb"
+};
+
